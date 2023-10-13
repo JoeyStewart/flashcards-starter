4757319dedfb5056d6b502ccf52c1f98db7b70e0
Joseph Stewart <JosephStewart1752@gmail.com>
2023-10-12 23:04:41 -0400

    TDD now work with proper assert now defined at head of page

--- test/Card-test.js
+++ test/Card-test.js
@@ -1,5 +1,5 @@
-const chai = require('chai');
-const expect = chai.expect;
+const { assert, expect } = require('chai');
+
 
 const { createCard, evaluateGuess, createDeck, createRound} = require('../src/card');
 
